4807741ff99e2c880040eefdec1d528b5417b0f5
Hidequel Puga <bounty31k@outlook.com>
2019-04-20 23:36:47 -0500

    commit msg 2019-04-20 11:36 pm

--- js/social-networks.js
+++ js/social-networks.js
@@ -1,23 +1,23 @@
 let socials = {
     facebook: {
       url: 'https://www.facebook.com/hidequel.puga',
-      icon: './img/facebook-icon.png'
+      icon: 'https://github.com/bounty31k/AllAboutMe/blob/master/img/facebook-icon.png'
     } ,
     youtube: {
       url: 'https://www.youtube.com/channel/UCanRFllGxt0r4yakALhHoog/featured?view_as=public',
-      icon: './img/youtube-icon.png'
+      icon: 'https://github.com/bounty31k/AllAboutMe/blob/master/img/youtube-icon.png'
     },
     twitter: { 
       url: 'https://twitter.com/bounty31k',
-      icon: './img/twitter-icon.png'
+      icon: 'https://github.com/bounty31k/AllAboutMe/blob/master/img/twitter-icon.png'
     },
     linkedin: {
       url: 'https://es.linkedin.com/in/hidequel-puga-48344654',
-      icon: './img/linkedin-icon.png'
+      icon: 'https://github.com/bounty31k/AllAboutMe/blob/master/img/linkedin-icon.png'
     },
     github: {
       url: 'https://github.com/bounty31k',
-      icon: './img/github-icon.png'
+      icon: 'https://github.com/bounty31k/AllAboutMe/blob/master/img/github-icon.png'
     } 
   }
   
